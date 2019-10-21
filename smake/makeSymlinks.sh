455d9c588be6a8832ca110a8a92ff992759797e3
salamynder <rtbtobi@gmail.com>
2019-10-21 15:27:45 +0200

    make symlinks shell sript

---
+++ smake/makeSymlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dotdir=~/git/dotfiles
+olddir=~/.dotfiles_old
+dfiles=$(ls -lA ../ |grep -v ^[di] | sed 's/\s\s*/ /g' | cut -d ' ' -f9)
+
+# backup place for maybe existing dot files
+echo -n "Creating backup folder $olddir for possibly existing dot files"
+mkdir -p $olddir
+echo "done"
+
+for dfile in $dfiles; do
+	mv ~/$dfile $olddir
+	echo "Creating symlink for $dfile in ~"
+	ln -s $dotdir/$dfile ~/$dfile
+done
